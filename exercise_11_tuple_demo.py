a615b104d7ce4875e58155e38d1bf04036443074
Reza <rezadelkabadi97@gmail.com>
2026-02-22 02:12:04 +0330

    Add exercise 11: tuple demonstration

---
+++ exercise_11_tuple_demo.py
@@ -0,0 +1,11 @@
+point = ((1,2),(3,4),(5,6))
+
+print("Noghte 2 : ",point[1])
+
+#point[0] = (2,2)
+print("Noghat : ")
+for i in point:
+    print(i)
+
+new_point = point + ((7,8),)
+print("Noghat jadid : ",new_point)
